c7a3c9ae8c35ead5c8a7ba5e52c07850201f3dda
anca291 <anca.ilie29@gmail.com>
2024-03-27 16:52:01 +0200

    Sprint2 Displaying travel card details

--- travel-journal-api/src/main/java/travel/journal/api/controller/TravelController.java
+++ travel-journal-api/src/main/java/travel/journal/api/controller/TravelController.java
@@ -2,19 +2,24 @@ package travel.journal.api.controller;
 
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import travel.journal.api.dto.travelJournal.inbound.TravelJournalDTO;
 import travel.journal.api.dto.travelJournal.outbound.TravelJournalDetailsDTO;
-import travel.journal.api.service.TravelService;
+import travel.journal.api.security.services.UserDetailsImpl;
 import travel.journal.api.service.TravelServiceImpl;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/travel-journal")
 public class TravelController {
     private final TravelServiceImpl travelServiceImpl;
@@ -34,8 +39,13 @@ public class TravelController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/travel/{id}")
-    public ResponseEntity<TravelJournalDetailsDTO> getTravel(@PathVariable("id") int travelId) {
-        TravelJournalDetailsDTO travelToGet = travelServiceImpl.getTravelJournal(travelId);
+    public ResponseEntity<TravelJournalDetailsDTO> getTravel(@PathVariable("id") @Positive int travelId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        Integer userId;
+        UserDetailsImpl userDetails = (UserDetailsImpl) principal;
+        userId = userDetails.getId();
+        TravelJournalDetailsDTO travelToGet = travelServiceImpl.getTravelJournal(travelId, userId);
         return ResponseEntity.ok(travelToGet);
     }
 
