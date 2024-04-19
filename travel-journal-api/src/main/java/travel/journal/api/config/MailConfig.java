513619e1b1af67a7d28a20b7992a245cc88af9a2
Stancu2001 <fstancu346@gmail.com>
2024-04-20 00:06:23 +0300

    Added the email credentials as environment variables and the reset password link has been changed to the one for FE.

--- travel-journal-api/src/main/java/travel/journal/api/config/MailConfig.java
+++ travel-journal-api/src/main/java/travel/journal/api/config/MailConfig.java
@@ -1,5 +1,6 @@
 package travel.journal.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,22 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${Email_Username}")
+    private String Email_Username;
+    @Value("${Email_Password}")
+    private String Email_Password;
+    @Value("${Email_Host}")
+    private String Email_Host;
+    @Value("${Email_Port}")
+    private int Email_Port;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setUsername("noreply@stancuf.ro");
-        mailSender.setPassword("Stancu2002");
-        mailSender.setPort(465);
-        mailSender.setHost("mail.stancuf.ro");
+        mailSender.setUsername(Email_Username);
+        mailSender.setPassword(Email_Password);
+        mailSender.setPort(Email_Port);
+        mailSender.setHost(Email_Host);
         mailSender.setDefaultEncoding("UTF-8");
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
