bbe6570f894261b5489ccbbfe8dd82c09e24245c
UnDrew <bandrayy@gmail.com>
2024-04-22 18:00:42 +0300

    Added appropriate content-type based on filename & added cache control

--- travel-journal-api/src/main/java/travel/journal/api/controller/FileController.java
+++ travel-journal-api/src/main/java/travel/journal/api/controller/FileController.java
@@ -1,9 +1,11 @@
 package travel.journal.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import travel.journal.api.entities.Files;
 import travel.journal.api.service.FileService;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,8 +31,12 @@ public class FileController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        headers.setContentDispositionFormData("fileName", image.getFileName());
+        String imageName = image.getFileName();
+        headers.setContentType(MediaTypeFactory.getMediaType(imageName).orElse(MediaType.APPLICATION_OCTET_STREAM));
+        headers.setContentDispositionFormData("fileName", imageName);
+        headers.setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS)
+                .noTransform()
+                .mustRevalidate());
         return new ResponseEntity<>(image.getFileContent(), headers, HttpStatus.OK);
     }
 
